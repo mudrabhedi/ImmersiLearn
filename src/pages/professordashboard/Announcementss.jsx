9ec46fcf2c9e275b72778bafbdd1ab2cbe1c5cef
Mudra Bhedi <127314892+mudrabhedi@users.noreply.github.com>
2025-04-06 05:14:07 +0530

    Update Announcementss.jsx

--- src/pages/professordashboard/Announcementss.jsx
+++ src/pages/professordashboard/Announcementss.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { FaPaperPlane, FaCalendarAlt, FaTrash } from "react-icons/fa";
 import SidePanelProf from "../../components/SidePanelProf";
 
