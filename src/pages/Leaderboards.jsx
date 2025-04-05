9f43849618b755caefb54015f3bfbb5e307c5ca9
Mudra Bhedi <127314892+mudrabhedi@users.noreply.github.com>
2025-04-06 04:45:03 +0530

    Update Leaderboards.jsx

--- src/pages/Leaderboards.jsx
+++ src/pages/Leaderboards.jsx
@@ -13,7 +13,7 @@ const Leaderboard = () => {
   useEffect(() => {
     const fetchLeaderboard = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/leaderboard');
+        const response = await fetch('https://immersilearn-backend.onrender.com/api/leaderboard');
         if (!response.ok) {
          
         }
@@ -38,7 +38,7 @@ const Leaderboard = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/leaderboard', {
+      const response = await fetch('https://immersilearn-backend.onrender.com/api/leaderboard', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, score: parseInt(score) }),
